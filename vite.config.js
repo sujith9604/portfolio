8a99193001edcf206cb75a9dfbf425cd40560f02
sujith9604 <sujith9604@gmail.com>
2025-07-13 00:00:33 +0530

    Router Resolved

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   plugins: [react()],
