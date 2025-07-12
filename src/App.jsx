40bdcc477527dc96e8d9be6093ad3d17a036e09a
sujith9604 <sujith9604@gmail.com>
2025-07-12 23:39:40 +0530

    Route Changed

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,6 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/" element={<Navigate to="/portfolio" replace />} />
         </Routes>
       </div>
     </Router>
