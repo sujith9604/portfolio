fde77d171cc68fe3612deb8782a04f20f6c903ab
sujith9604 <sujith9604@gmail.com>
2025-07-12 23:33:10 +0530

    Route Changed

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
         <ParticleBackground />
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/portfolio" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
